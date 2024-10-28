a3f6e6066e92eca60193cab7051083c8e2672864
kasra10 <kasrasparrow@gmail.com>
2024-10-28 19:48:05 +0330

    Add Flutter Navigator and Fix The Bug Of COntext as well. Add Page TwoScreen :))

--- lib/main.dart
+++ lib/main.dart
@@ -3,105 +3,116 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import './src/app_toast_message.dart';
 import 'package:hugeicons/hugeicons.dart';
+import './src/category_page.dart';
 
-void main() => runApp(const QuizGame());
+void main() => runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        color: Colors.white,
+        home: QuizGame(),
+      ),
+    );
 
 class QuizGame extends StatelessWidget {
   const QuizGame({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
           backgroundColor: Colors.white,
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            toolbarHeight: 70.0,
-            centerTitle: true,
-            leading: IconButton(
-              onPressed: () => exit(0),
+          toolbarHeight: 70.0,
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () => exit(0),
+            icon: const HugeIcon(
+              icon: HugeIcons.strokeRoundedArrowTurnBackward,
+              color: Colors.black,
+            ),
+            tooltip: 'Exit From The App!',
+            enableFeedback: true,
+          ),
+          title: const Text(
+            'Quiz Time',
+            style: TextStyle(
+              fontFamily: 'LexendExa',
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () => showAppToast(
+                'coming soon...'.toUpperCase(),
+              ),
               icon: const HugeIcon(
-                icon: HugeIcons.strokeRoundedArrowTurnBackward,
+                icon: HugeIcons.strokeRoundedUserAccount,
                 color: Colors.black,
               ),
-              tooltip: 'Exit From The App!',
+              tooltip: 'Go To Account Center',
               enableFeedback: true,
-            ),
-            title: const Text(
-              'Quiz Time',
-              style: TextStyle(
-                fontFamily: 'LexendExa',
-                fontSize: 22,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () => showAppToast(
-                  'coming soon...'.toUpperCase(),
-                ),
-                icon: const HugeIcon(
-                  icon: HugeIcons.strokeRoundedUserAccount,
-                  color: Colors.black,
-                ),
-                tooltip: 'Go To Account Center',
-                enableFeedback: true,
-              )
-            ], // End Of <Widget> Actions
-          ),
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 168,
-                    height: 168,
-                    child: Image.asset(
-                      'images/orbitQuiz.png',
-                      semanticLabel: 'An Orbit/earth having badge of Question',
-                    ),
+            )
+          ], // End Of <Widget> Actions
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 168,
+                  height: 168,
+                  child: Image.asset(
+                    'images/orbitQuiz.png',
+                    semanticLabel: 'An Orbit/earth having badge of Question',
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(
-                      15.5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(
+                    15.5,
+                  ),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CategoryPage(),
+                        ),
+                      );
+                    },
+                    label: Text(
+                      'Start',
+                      style: TextStyle(
+                        fontFamily: 'LexendExa',
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      label: Text(
-                        'Start',
-                        style: TextStyle(
-                          fontFamily: 'LexendExa',
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      elevation: 0.0,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 115.5,
+                        vertical: 10.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            25.0,
+                          ),
+                        ),
+                        side: BorderSide(
                           color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        elevation: 0.0,
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 115.5,
-                          vertical: 10.0,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(
-                              25.0,
-                            ),
-                          ),
-                          side: BorderSide(
-                            color: Colors.black,
-                            width: 2.0,
-                          ),
+                          width: 2.0,
                         ),
                       ),
                     ),
                   ),
-                ], // End Of Column Children
-              ),
+                ),
+              ], // End Of Column Children
             ),
           ),
         ),
---
+++ lib/src/category_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        toolbarHeight: 70.0,
+        centerTitle: true,
+        leading: BackButton(
+          onPressed: () => Navigator.pop(context),
+          color: Colors.white,
+        ),
+        title: const Text(
+          'Back',
+          style: TextStyle(
+              fontFamily: 'LexendExa',
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+              color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
