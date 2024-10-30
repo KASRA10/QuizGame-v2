6cc9a110887b435c5057376e8f70f59e12309330
kasra10 <kasrasparrow@gmail.com>
2024-10-30 14:04:12 +0330

    Add Some Items on categories Page

--- lib/src/category_page.dart
+++ lib/src/category_page.dart
@@ -51,6 +51,167 @@ class CategoryPage extends StatelessWidget {
             ),
           ],
         ),
+        body: Column(
+          children: [
+            SizedBox(
+              width: 168.0,
+              height: 168.0,
+              child: Image.asset(
+                'images/categories_image.png',
+                semanticLabel:
+                    'categories icons indicated by some distinct shapes',
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Sport',
+                style: TextStyle(
+                  fontFamily: 'LexendExa',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+              iconAlignment: IconAlignment.end,
+              icon: Icon(
+                HugeIcons.strokeRoundedBoxingGlove01,
+                color: Colors.grey.shade900,
+                semanticLabel: 'Boxing Gloves Indicating sport category',
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.5,
+                  vertical: 10.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      25.0,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Colors.grey.shade900,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Sport',
+                style: TextStyle(
+                  fontFamily: 'LexendExa',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+              iconAlignment: IconAlignment.end,
+              icon: Icon(
+                HugeIcons.strokeRoundedBoxingGlove01,
+                color: Colors.grey.shade900,
+                semanticLabel: 'Boxing Gloves Indicating sport category',
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.5,
+                  vertical: 10.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      25.0,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Colors.grey.shade900,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Sport',
+                style: TextStyle(
+                  fontFamily: 'LexendExa',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+              iconAlignment: IconAlignment.end,
+              icon: Icon(
+                HugeIcons.strokeRoundedBoxingGlove01,
+                color: Colors.grey.shade900,
+                semanticLabel: 'Boxing Gloves Indicating sport category',
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.5,
+                  vertical: 10.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      25.0,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Colors.grey.shade900,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(
+                'Sport',
+                style: TextStyle(
+                  fontFamily: 'LexendExa',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+              iconAlignment: IconAlignment.end,
+              icon: Icon(
+                HugeIcons.strokeRoundedBoxingGlove01,
+                color: Colors.grey.shade900,
+                semanticLabel: 'Boxing Gloves Indicating sport category',
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.5,
+                  vertical: 10.0,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      25.0,
+                    ),
+                  ),
+                  side: BorderSide(
+                    color: Colors.grey.shade900,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+            ),
+          ], // End Of Children
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: FloatingActionButton(
           onPressed: () => searchAlert(context),
