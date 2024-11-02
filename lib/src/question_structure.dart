ea87730ad63fe4b0dfb82d6bdec015794af04247
kasra10 <kasrasparrow@gmail.com>
2024-11-02 13:53:26 +0330

    add Question class and Sport Question list

---
+++ lib/src/question_structure.dart
@@ -0,0 +1,9 @@
+class QuestionStructure {
+  late String question;
+  late bool answer;
+
+  QuestionStructure({
+    required question,
+    required answer,
+  });
+}
---
+++ lib/src/sport_questions_list.dart
@@ -0,0 +1,88 @@
+import './question_structure.dart';
+
+List<QuestionStructure> sportQuestionsList = [
+  QuestionStructure(
+    question: 'The Olympic Games are held every four years.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Soccer is the most popular sport worldwide.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Michael Jordan played for the Los Angeles Lakers during his NBA career.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'The Super Bowl is the championship game of the National Football League (NFL).',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Tennis matches can end in a tie.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'The Tour de France is an annual men\'s bicycle race primarily held in France.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The FIFA World Cup takes place every two years.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Usain Bolt holds the world record for the 100 meters sprint.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Cricket originated in Australia.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'The New York Yankees are a Major League Baseball team.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'A marathon is 42.195 kilometers long.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The sport of fencing is always done with a sword.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The term "offside" is used in basketball.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'The Olympic rings represent five continents.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Swimming is included in the Summer Olympics.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'In American football, a touchdown is worth six points.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Rugby union and rugby league are the same sport.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'In golf, a hole-in-one means the ball is hit into the hole on the first stroke.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The NBA was founded in 1946.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Ice hockey is played on a field rather than an ice rink.',
+    answer: false,
+  ),
+];
