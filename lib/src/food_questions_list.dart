4036e18c8928672d4e6c655f50b19123e9c325c4
kasra10 <kasrasparrow@gmail.com>
2024-11-02 14:03:19 +0330

    add Food Questions list

---
+++ lib/src/food_questions_list.dart
@@ -0,0 +1,90 @@
+import './question_structure.dart';
+
+List<QuestionStructure> foodQuestionsList = [
+  QuestionStructure(
+    question: 'Sushi is traditionally made with raw fish.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'All cheeses are made from cow\'s milk.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Chocolate is derived from cocoa beans.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Pasta is a staple food in Italian cuisine.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Ceviche is a dish made from cooked fish.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Quinoa is considered a grain, but it\'s actually a seed.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Olive oil is primarily used for frying in Mediterranean cooking.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Falafel is made from ground chickpeas or fava beans.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The fruit that is often mistaken for a vegetable is the tomato.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Waffles and pancakes are made from the same batter.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Prosciutto is a type of hard cheese.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Basil is commonly used as a herb in Italian cooking.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Fondue is a Swiss dish made by melting cheese.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Hummus is primarily made from peanuts.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'Kimchi is a traditional Korean dish made from fermented vegetables.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The spice saffron is derived from the flower of Crocus sativus.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Bread is a staple food in every culture around the world.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Cinnamon is made from the inner bark of trees.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'A serving of avocado is high in carbohydrates.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Persimmons are known for their sweet and honey-like flavor.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Ramen is a traditional dish from Thailand.',
+    answer: false,
+  ),
+];
