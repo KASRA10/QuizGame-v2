25b8e2136a65cac0d468b30c5e08f7a4e5b8b7ba
kasra10 <kasrasparrow@gmail.com>
2024-11-02 14:08:30 +0330

    add Technology Questions list

---
+++ lib/src/technology_questions_list.dart
@@ -0,0 +1,95 @@
+import './question_structure.dart';
+
+List<QuestionStructure> technologyQuestionsList = [
+  QuestionStructure(
+    question: 'The first programmable computer was developed in the 1940s.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'HTML stands for HyperText Markup Language.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Python is a type of machine learning algorithm.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'The iPhone was first released in 2007.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Bluetooth technology is used for wired connections.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'Cloud computing allows users to store and access data over the internet.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Java is a programming language that was developed by Microsoft.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'Artificial Intelligence (AI) refers to the simulation of human intelligence in machines.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The World Wide Web was invented by Tim Berners-Lee.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Virtual Reality (VR) is the same as Augmented Reality (AR).',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'Blockchain technology is primarily associated with cryptocurrencies.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'A USB drive is a type of optical storage.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'The programming language C++ is an extension of the programming language C.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Cybersecurity is the practice of protecting systems from digital attacks.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Wi-Fi stands for Wireless Fidelity.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'A motherboard is the main circuit board in a computer.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Quantum computing uses classical bits to process information.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'GPS stands for Global Positioning System.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The term "IoT" stands for Internet of Trees.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        '3D printing allows for the creation of three-dimensional objects.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Laptops are typically lighter and more portable than desktop computers.',
+    answer: true,
+  ),
+];
