d4e52e479d223ea1c4b8be5ab5b6097596dcd391
kasra10 <kasrasparrow@gmail.com>
2024-11-02 13:57:52 +0330

    add movie questions list

---
+++ lib/src/movie_questions_list.dart
@@ -0,0 +1,95 @@
+import './question_structure.dart';
+
+List<QuestionStructure> movieQuestionsList = [
+  QuestionStructure(
+    question:
+        'The Godfather is often considered one of the greatest films of all time.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Titanic won the Academy Award for Best Picture in 1998.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Leonardo DiCaprio won his first Oscar for his role in The Revenant.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The Matrix was released in the 1990s.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The character Jack Sparrow appears in the Harry Potter series.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Pulp Fiction was directed by Quentin Tarantino.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Avatar is the highest-grossing film of all time (as of 2023).',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The film Inception is about time travel.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Harrison Ford played the character Indiana Jones.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Frozen is a Disney film featuring the song "Let It Go."',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Jaws was the first film to gross more than One billion worldwide.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'The Shawshank Redemption is based on a novella by Stephen King.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Jurassic Park features dinosaurs created from DNA.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'The Wizard of Oz was released in the 1980s.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Marvel Cinematic Universe began with Iron Man.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'The Lord of the Rings trilogy was directed by Christopher Nolan.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question: 'Coco is a film that explores the theme of Dia de los Muertos.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'Forrest Gump features the quote, "Life is like a box of chocolates."',
+    answer: true,
+  ),
+  QuestionStructure(
+    question:
+        'The film Gladiator won the Academy Award for Best Picture in 2000.',
+    answer: true,
+  ),
+  QuestionStructure(
+    question: 'Finding Nemo is a live-action film.',
+    answer: false,
+  ),
+  QuestionStructure(
+    question:
+        'The Dark Knight features the character Joker, played by Heath Ledger.',
+    answer: true,
+  ),
+];
